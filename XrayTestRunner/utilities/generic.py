85fd023e46e9ed658077e102f5d95395dabc6a77
JulianWolf <Julian.wolf2@dxc.com>
2025-02-24 12:39:20 +0100

    fix: Some utility functions

--- XrayTestRunner/utilities/generic.py
+++ XrayTestRunner/utilities/generic.py
@@ -5,9 +5,12 @@ import re
 def log_iteration_result(location:str, iteration:int, custom:str=""):
 	def decorator(func):
 		def wrapper(self, *args, **kwargs):
-			func(self, *args, **kwargs)
 			storagePlan=LocalFileStorage()
 			xtrf=XrayTestRunnerFile(storage=storagePlan)
+			try:
+				func(self, *args, **kwargs)
+			except AssertionError as ae:
+				pass # NOP
 			xtrf.save(
 				name=f"{getattr(self,"_testMethodDoc").strip().split('\n')[0]} iteration {iteration+1} {custom}.json",
 				location=location,
@@ -16,8 +19,7 @@ def log_iteration_result(location:str, iteration:int, custom:str=""):
 		return wrapper
 	return decorator
 
-def set_value(data: dict, key: str, val: any):
-	if val=="": return
+def set_value(data: dict, key: str, val: any, default: dict | list={}):
 	if '.' in key:
 		f1, f2 = key.split('.', 1)  # Split only once
 		match = re.match(r'(\d+)\.(\w+)', f2)  # Check if f1 contains list indexing
@@ -28,6 +30,11 @@ def set_value(data: dict, key: str, val: any):
 			
 			data[f1][index][list_value] = val  # Modify the existing dict inside list
 		else:
-			data.setdefault(f1, {})[f2] = val
+			match default:
+				case {}:
+					data.setdefault(f1, {})[f2] = val
+				case []:
+					data.setdefault(f1, []).push(val)
+
 	else:
 		data[key] = val
